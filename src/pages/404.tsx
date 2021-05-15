4625579b3fd70cb09ed36beb952262711baea4bc
pa <pa@MacBook-Pro-pa.local>
2021-05-15 15:38:17 +0200

    chore: 404 fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,10 @@ import { NoDataTemplate } from '@ui/templates';
 
 type Props = {};
 
+const browser = typeof window !== 'undefined' && window;
+
 const NotFoundPage: React.FC<Props> = ({}) => {
-  return <NoDataTemplate />;
+  return browser ? <NoDataTemplate /> : null;
 };
 
 export default NotFoundPage;
